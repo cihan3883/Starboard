d980dcf29e8d230da1f6531e1b4cdf2e568b1bdb
Glitch (testbotgmm) <none>
2018-08-04 06:47:27 +0000

    🗻👩‍❤️‍💋‍👩 Checkpoint ./server.js:1406671/2627 ./info-base.json:1406671/2 ./info.json:1406671/2

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var randomWords = require('random-words');
 var FS = require('file-system');
 
-var info = FS.readFile(
+var info = JSON.parse(FS.readFileSync("info.json"));
+console.log("Loaded JSON:");
+console.log(info);
+
+var orgMsg = "";
+var orgName = "";
 
 //Discord
 const Discord = require('discord.js');
@@ -81,11 +86,31 @@ bot.on("message", (message) => {
       //To string
       newMsg = words.join(' ');
 
-      //Add name
-      newMsg = "**" + message.author.username + "**: " + newMsg;
+      //Name
+      let name = "**" + message.author.username + "**";
 
       //Send
-      message.channel.send(newMsg);
+      //Entering first message
+      if (orgMsg==""){
+        orgMsg = msg;
+        orgName = message.author.username;
+        
+        message.channel.send(name + ": " + newMsg);
+        message.channel.send("One or two words have been replaced. Guess what they originally said!");
+      }
+      //Guessing
+      else{
+        //Correct
+        if (msg.toLowerCase() == orgMsg.toLowerCase()){
+          message.channel.send(name + ", that's right! You win.");
+          orgMsg = "";
+          orgName = "";
+          message.channel.send("Now someone say something so that I can mess it up!");
+        }
+        else{
+          
+        }
+      }
 
       //Delete
       message.delete(0);
@@ -104,49 +129,17 @@ bot.login(process.env.TOKEN);
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// init sqlite db
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
-    
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
-  }
-});
-
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
-});
-
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//Save
+function save(){
+  FS.writeFile("info.json", info);
+}
