2bd336822373b58c169d90fc9d76694064670e3f
Glitch (discord-js-bot-template) <none>
2019-11-07 23:05:26 +0000

    💡📅 Checkpoint ./server.js:2771570/43

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-var randomWords = require('random-words');
 var FS = require('file-system');
 
 var info = JSON.parse(FS.readFileSync("info.json"));
