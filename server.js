95706c59e3023df502d7410223a5eddc50af5e0c
Glitch (discord-js-bot-template) <none>
2019-11-08 01:17:25 +0000

    🏔💆 Checkpoint ./server.js:2771570/353 ./data.json:2771570/20

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Init project
 const FS = require('file-system');
 let config = JSON.parse(FS.readFileSync("config.json"));
+let data ) JSON.parse
 
 // Discord
 const Discord = require('discord.js');
@@ -23,24 +24,23 @@ Client.on("MessageReactionAdd", async (reaction, user) => {
   if (message.author.bot)
     return message.channel.send(`${user}, you cannot star bot messages.`);
   
-  let starChannel = message.guild.channels.find(channel => channel.name === config.starboardChannel);
-  let fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
-  const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
-  if (stars) {
-    const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
-    const foundStar = stars.embeds[0];
-    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
-    const embed = new Discord.RichEmbed()
-      .setColor(foundStar.color)
-      .setDescription(foundStar.description)
+  let starboard = message.guild.channels.find(channel => channel.name === config.starboardChannel);
+  let fetchedMessages = await starboard.fetchMessages({ limit: 100 });
+  let starboardMessage = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
+  if (starboardMessage !== undefined) {
+    let stars = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(starboardMessage.embeds[0].footer.text);
+    let embed = starboardMessage.embeds[0];
+    let image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+    const newEmbed = new Discord.RichEmbed()
+      .setColor(embed.color)
+      .setDescription(embed.description)
       .setAuthor(message.author.tag, message.author.displayAvatarURL)
       .setTimestamp()
-      .setFooter(`⭐ ${parseInt(star[1])+1} | ${message.id}`)
+      .setFooter(`⭐ ${parseInt(stars[1])+1} | ${message.id}`)
       .setImage(image);
-    const starMsg = await starChannel.fetchMessage(stars.id);
+    const starMsg = await starboard.fetchMessage(starboardMessage.id);
     await starMsg.edit({ embed });
-  }
-  if (!stars) {
+  } else {
     const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
     if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
     const embed = new Discord.RichEmbed()
@@ -50,7 +50,7 @@ Client.on("MessageReactionAdd", async (reaction, user) => {
       .setTimestamp(new Date())
       .setFooter(`⭐ 1 | ${message.id}`)
       .setImage(image);
-    await starChannel.send({ embed });
+    await starboard.send({ embed });
   }
   
 });
@@ -61,5 +61,5 @@ Client.login(process.env.TOKEN);
 
 // Save
 function save() {
-  FS.writeFile("info.json", JSON.stringify(info));
+  FS.writeFile("data.json", JSON.stringify(data));
 }
