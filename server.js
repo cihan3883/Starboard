7f397dd42956483758b98922f2725fdd6834cc11
Glitch (discord-js-bot-template) <none>
2019-11-08 01:29:24 +0000

    🎊🏄 Checkpoint ./server.js:2771570/978 ./config.json:2771570/34

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Init project
 const FS = require('file-system');
 let config = JSON.parse(FS.readFileSync("config.json"));
-let data ) JSON.parse
+let data = JSON.parse(FS.readFileSync("data.json"));
 
 // Discord
 const Discord = require('discord.js');
@@ -27,24 +27,25 @@ Client.on("MessageReactionAdd", async (reaction, user) => {
   let starboard = message.guild.channels.find(channel => channel.name === config.starboardChannel);
   let fetchedMessages = await starboard.fetchMessages({ limit: 100 });
   let starboardMessage = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
+  
   if (starboardMessage !== undefined) {
-    let stars = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(starboardMessage.embeds[0].footer.text);
+    let starAmount = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(starboardMessage.embeds[0].footer.text);
     let embed = starboardMessage.embeds[0];
-    let image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+    let image = message.attachments.size > 0 ? await extension(message.attachments.array()[0].url) : '';
     const newEmbed = new Discord.RichEmbed()
       .setColor(embed.color)
       .setDescription(embed.description)
       .setAuthor(message.author.tag, message.author.displayAvatarURL)
       .setTimestamp()
-      .setFooter(`⭐ ${parseInt(stars[1])+1} | ${message.id}`)
+      .setFooter(`⭐ ${parseInt(starAmount[1])+1} | ${message.id}`)
       .setImage(image);
-    const starMsg = await starboard.fetchMessage(starboardMessage.id);
-    await starMsg.edit({ embed });
+    let starMsg = await starboard.fetchMessage(starboardMessage.id);
+    await starMsg.edit({newEmbed} );
   } else {
-    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+    const image = message.attachments.size > 0 ? await this.extension(message.attachments.array()[0].url) : '';
     if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
     const embed = new Discord.RichEmbed()
-      .setColor(15844367)
+      .setColor(config.defaultColour)
       .setDescription(message.cleanContent)
       .setAuthor(message.author.tag, message.author.displayAvatarURL)
       .setTimestamp(new Date())
@@ -55,6 +56,13 @@ Client.on("MessageReactionAdd", async (reaction, user) => {
   
 });
 
+function extension(attachment) {
+    let imageLink = attachment.split('.');
+    let typeOfImage = imageLink[imageLink.length - 1];
+    let image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
+    if (!image) return '';
+    return attachment;
+}
 
 // Log in bot
 Client.login(process.env.TOKEN);
