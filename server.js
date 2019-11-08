988a9861f957a73b43bbd3dad2b72d423ee3926a
Glitch (discord-js-bot-template) <none>
2019-11-08 14:47:47 +0000

    🏥🍈 Checkpoint ./server.js:2771570/154

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ async function checkReaction(reaction, user, starAmount) {
       .setColor(embed.color)
       .setDescription(embed.description)
       .setAuthor(message.author.tag, message.author.displayAvatarURL())
-      .addField('Original Message', ``)
+      .addField('Original Message', `[view](${message.url})`)
       .setTimestamp()
       .setFooter(`⭐ ${parseInt(starCount[1]) + starAmount} | ${message.id}`);
     
@@ -112,6 +112,7 @@ async function checkReaction(reaction, user, starAmount) {
         .setColor(config.defaultColour)
         .setDescription(message.cleanContent)
         .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .addField('Original Message', `[view](${message.url})`)
         .setTimestamp(new Date())
         .setFooter(`⭐ ${starCount} | ${message.id}`);
       
