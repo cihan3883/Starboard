d756f635c7f8736ab9058769d48d8b74b67c5cde
Glitch (discord-js-bot-template) <none>
2019-11-08 00:17:25 +0000

    😺⛩ Checkpoint ./server.js:2771570/843 ./info.json:2771570/72

--- server.js
+++ server.js
@@ -18,29 +18,7 @@ Client.on("ready", () => {
   console.log("Bot started");
 });
 
-Client.on("message", (message) => {
-  let msg = message.content;
-  let channel = message.channel.name;
-  
-  //Commands
-  if (msg.substring(0, 1)=="!") { //Command prefix, like !command
-    let args = msg.slice(1).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
-    
-    //Commands
-    switch (command) { //Commands here
-      case "hi": //Example of a simple command. Use !hi and it says "hi"
-        message.channel.send("hi");
-      break;
-    }
-  }
-  //Message replies
-  else if (message.author.id != "465945834517823488") { //Replace this number with your bot's user ID
-    if (msg=="hello bot") { //If someone says "hello bot",
-      message.channel.send("hello human"); //say "hello human"
-    }
-  }
-});
+
 
 Client.login(process.env.TOKEN);
 
@@ -60,9 +38,4 @@ var listener = app.listen(process.env.PORT, function () {
 //Save
 function save() {
   FS.writeFile("info.json", JSON.stringify(info));
-}
-
-//Choose a random item from an array
-function choose(arr) {
-  return arr[Math.floor(Math.random()*arr.length)];
 }
