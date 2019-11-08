728c842dd6da3c5bffa10bca0a3494d1e74eaa06
Glitch (discord-js-bot-template) <none>
2019-11-08 00:29:26 +0000

    🎡🏊🏼 Checkpoint ./info.json:2771570/17 ./server.js:2771570/641

--- server.js
+++ server.js
@@ -1,41 +1,22 @@
-// server.js
-// where your node app starts
+// Init project
+const FS = require('file-system');
+let info = JSON.parse(FS.readFileSync("info.json"));
 
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-var FS = require('file-system');
-
-var info = JSON.parse(FS.readFileSync("info.json"));
-
-//Discord
+// Discord
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 
 Client.on("ready", () => {
-  console.log("Bot started");
+  console.log("StarBot started");
 });
 
 
 
+
+// Log in bot
 Client.login(process.env.TOKEN);
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
-//Save
+// Save
 function save() {
   FS.writeFile("info.json", JSON.stringify(info));
 }
