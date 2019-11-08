24310eb877662b4db5f6a7327110a5cc8ab55a16
Glitch (discord-js-bot-template) <none>
2019-11-08 00:53:25 +0000

    🙎💴 Checkpoint ./server.js:2771570/21053 ./info.json:2771570/2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Init project
 const FS = require('file-system');
-let info = JSON.parse(FS.readFileSync("info.json"));
+let config = JSON.parse(FS.readFileSync("config.json"));
 
 // Discord
 const Discord = require('discord.js');
@@ -12,41 +12,48 @@ Client.on("ready", () => {
 
 
 Client.on("MessageReactionAdd", async (reaction, user) => {
-  const message = reaction.message;
-    if (reaction.emoji.name !== '⭐') return;
-    if (message.author.id === user.id) return message.channel.send(`${user}, you cannot star your own messages.`);
-    if (message.author.bot) return message.channel.send(`${user}, you cannot star bot messages.`);
-    const { starboardChannel } = this.client.settings.get(message.guild.id);
-    const starChannel = message.guild.channels.find(channel => channel.name === starboardChannel)
-    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
-    const fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
-    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
-    if (stars) {
-      const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
-      const foundStar = stars.embeds[0];
-      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
-      const embed = new RichEmbed()
-        .setColor(foundStar.color)
-        .setDescription(foundStar.description)
-        .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        .setTimestamp()
-        .setFooter(`⭐ ${parseInt(star[1])+1} | ${message.id}`)
-        .setImage(image);
-      const starMsg = await starChannel.fetchMessage(stars.id);
-      await starMsg.edit({ embed });
-    }
-    if (!stars) {
-      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
-      if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
-      const embed = new RichEmbed()
-        .setColor(15844367)
-        .setDescription(message.cleanContent)
-        .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        .setTimestamp(new Date())
-        .setFooter(`⭐ 1 | ${message.id}`)
-        .setImage(image);
-      await starChannel.send({ embed });
-    }
+  let message = reaction.message;
+  
+  // Reaction isn't a star
+  if (reaction.emoji.name !== '⭐') return;
+  // Reacting to your own message
+  if (message.author.id === user.id)
+    return message.channel.send(`${user}, you cannot star your own messages.`);
+  // Message is from a bot
+  if (message.author.bot)
+    return message.channel.send(`${user}, you cannot star bot messages.`);
+  
+  let starboardChannel = config.starboardChannel;
+  const starChannel = message.guild.channels.find(channel => channel.name === starboardChannel)
+  if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+  const fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
+  const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
+  if (stars) {
+    const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+    const foundStar = stars.embeds[0];
+    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+    const embed = new Discord.RichEmbed()
+      .setColor(foundStar.color)
+      .setDescription(foundStar.description)
+      .setAuthor(message.author.tag, message.author.displayAvatarURL)
+      .setTimestamp()
+      .setFooter(`⭐ ${parseInt(star[1])+1} | ${message.id}`)
+      .setImage(image);
+    const starMsg = await starChannel.fetchMessage(stars.id);
+    await starMsg.edit({ embed });
+  }
+  if (!stars) {
+    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
+    if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
+    const embed = new Discord.RichEmbed()
+      .setColor(15844367)
+      .setDescription(message.cleanContent)
+      .setAuthor(message.author.tag, message.author.displayAvatarURL)
+      .setTimestamp(new Date())
+      .setFooter(`⭐ 1 | ${message.id}`)
+      .setImage(image);
+    await starChannel.send({ embed });
+  }
   
 });
 
